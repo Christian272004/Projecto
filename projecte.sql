21f8b908dfbef879ab57d7988d9646d6f0dbd9c5
Christian272004 <165886314+Christian272004@users.noreply.github.com>
2024-12-05 14:36:35 +0100

    modificació del fichero sql

--- projecte.sql
+++ projecte.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 21-11-2024 a las 20:14:07
+-- Tiempo de generación: 05-12-2024 a las 14:17:17
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -69,7 +69,10 @@ CREATE TABLE `informacio_viatge` (
 --
 
 INSERT INTO `informacio_viatge` (`Data`, `continent`, `pais`, `preu_persona`, `preu_viatge`, `Nom`, `Telefon`, `Persones`, `Descompte`, `Id`) VALUES
-('2024-11-23', '5', '22', 1600, 3200, 'Marcos', '6546825141', '2', 0, 36);
+('2024-11-28', '2', 'Estados Unidos', 800, 800, 'Pedro', '6546825141', '1', 0, 43),
+('2024-11-28', '4', 'España', 150, 300, 'Pedro', '6546825141', '2', 0, 44),
+('2024-12-08', '3', 'India', 1000, 1, 'Pedro', '6546825141', '1', 0, 45),
+('2025-01-01', '1', 'Egipto', 600, 600, 'Pedro', '6546825141', '1', 0, 47);
 
 -- --------------------------------------------------------
 
@@ -99,67 +102,23 @@ INSERT INTO `paises` (`id_pais`, `nombre_pais`, `id_continente`, `precio_por_per
 (7, 'Brasil', 2, 950.00, 'Vista/imagenes/Brasil.jpg'),
 (8, 'México', 2, 850.00, 'Vista/imagenes/Mexico.jpg'),
 (9, 'Argentina', 2, 1000.00, 'Vista/imagenes/Argentina.jpg'),
-(11, 'China', 3, 1300.00, 'Vista/imagenes/China.jpg'),
-(12, 'Japón', 3, 1400.00, 'Vista/imagenes/Japon.jpg'),
-(13, 'India', 3, 1000.00, 'Vista/imagenes/India.jpg'),
-(14, 'Corea del Sur', 3, 1200.00, 'Vista/imagenes/CoreaSur.jpg'),
-(15, 'Tailandia', 3, 1100.00, 'Vista/imagenes/Tailandia.jpg'),
-(16, 'Francia', 4, 200.00, 'Vista/imagenes/Francia.jpg'),
-(17, 'España', 4, 150.00, 'Vista/imagenes/Espana.jpg'),
-(18, 'Alemania', 4, 300.00, 'Vista/imagenes/Alemania.jpg'),
-(19, 'Italia', 4, 250.00, 'Vista/imagenes/Italia.jpg'),
-(20, 'Reino Unido', 4, 400.00, 'Vista/imagenes/ReinoUnido.jpg'),
-(21, 'Australia', 5, 1500.00, 'Vista/imagenes/Australia.jpg'),
-(22, 'Nueva Zelanda', 5, 1600.00, 'Vista/imagenes/NuevaZelanda.jpg'),
-(23, 'Fiyi', 5, 1700.00, 'Vista/imagenes/Fiji.jpg'),
-(24, 'Papúa Nueva Guinea', 5, 1800.00, 'Vista/imagenes/NuevaGuinea.jpg'),
-(25, 'Samoa', 5, 1750.00, 'Vista/imagenes/Samoa.jpg'),
-(30, 'Canadá', NULL, 0.00, 'Vista/imagenes/Canada.jpg'),
-(31, 'China', NULL, 0.00, 'Vista/imagenes/China.jpg'),
-(32, 'Corea del Sur', NULL, 0.00, 'Vista/imagenes/CoreaSur.jpg'),
-(33, 'Egipto', NULL, 0.00, 'Vista/imagenes/Egipto.jpg'),
-(34, 'España', NULL, 0.00, 'Vista/imagenes/España.jpg'),
-(35, 'Estados Unidos', NULL, 0.00, 'Vista/imagenes/EEUU.jpg'),
-(36, 'Fiji', NULL, 0.00, 'Vista/imagenes/Fiji.jpg'),
-(37, 'Francia', NULL, 0.00, 'Vista/imagenes/Francia.jpg'),
-(38, 'India', NULL, 0.00, 'Vista/imagenes/India.jpg'),
-(39, 'Italia', NULL, 0.00, 'Vista/imagenes/Italia.jpg'),
-(40, 'Japón', NULL, 0.00, 'Vista/imagenes/Japon.jpg'),
-(41, 'Kenia', NULL, 0.00, 'Vista/imagenes/Kenia.jpg'),
-(42, 'Marruecos', NULL, 0.00, 'Vista/imagenes/Marruecos.jpg'),
-(43, 'México', NULL, 0.00, 'Vista/imagenes/Mexico.jpg'),
-(44, 'Nigeria', NULL, 0.00, 'Vista/imagenes/Nigeria.jpg'),
-(45, 'Nueva Zelanda', NULL, 0.00, 'Vista/imagenes/NuevaZelanda.jpg'),
-(46, 'Nueva Guinea', NULL, 0.00, 'Vista/imagenes/NuevaGuinea.jpg'),
-(47, 'Reino Unido', NULL, 0.00, 'Vista/imagenes/ReinoUnido.jpg'),
-(48, 'Samoa', NULL, 0.00, 'Vista/imagenes/Samoa.jpg'),
-(49, 'Sudáfrica', NULL, 0.00, 'Vista/imagenes/Sudafrica.jpg'),
-(50, 'Tailandia', NULL, 0.00, 'Vista/imagenes/Tailandia.jpg'),
-(51, 'Alemania', NULL, 0.00, 'Vista/imagenes/Alemania.jpg'),
-(52, 'Argentina', NULL, 0.00, 'Vista/imagenes/Argentina.jpg'),
-(53, 'Australia', NULL, 0.00, 'Vista/imagenes/Australia.jpg'),
-(54, 'Brasil', NULL, 0.00, 'Vista/imagenes/Brasil.jpg'),
-(55, 'Canadá', NULL, 0.00, 'Vista/imagenes/Canada.jpg'),
-(56, 'China', NULL, 0.00, 'Vista/imagenes/China.jpg'),
-(57, 'Corea del Sur', NULL, 0.00, 'Vista/imagenes/CoreaSur.jpg'),
-(58, 'Egipto', NULL, 0.00, 'Vista/imagenes/Egipto.jpg'),
-(59, 'España', NULL, 0.00, 'Vista/imagenes/España.jpg'),
-(60, 'Estados Unidos', NULL, 0.00, 'Vista/imagenes/EEUU.jpg'),
-(61, 'Fiji', NULL, 0.00, 'Vista/imagenes/Fiji.jpg'),
-(62, 'Francia', NULL, 0.00, 'Vista/imagenes/Francia.jpg'),
-(63, 'India', NULL, 0.00, 'Vista/imagenes/India.jpg'),
-(64, 'Italia', NULL, 0.00, 'Vista/imagenes/Italia.jpg'),
-(65, 'Japón', NULL, 0.00, 'Vista/imagenes/Japon.jpg'),
-(66, 'Kenia', NULL, 0.00, 'Vista/imagenes/Kenia.jpg'),
-(67, 'Marruecos', NULL, 0.00, 'Vista/imagenes/Marruecos.jpg'),
-(68, 'México', NULL, 0.00, 'Vista/imagenes/Mexico.jpg'),
-(69, 'Nigeria', NULL, 0.00, 'Vista/imagenes/Nigeria.jpg'),
-(70, 'Nueva Zelanda', NULL, 0.00, 'Vista/imagenes/NuevaZelanda.jpg'),
-(71, 'Nueva Guinea', NULL, 0.00, 'Vista/imagenes/NuevaGuinea.jpg'),
-(72, 'Reino Unido', NULL, 0.00, 'Vista/imagenes/ReinoUnido.jpg'),
-(73, 'Samoa', NULL, 0.00, 'Vista/imagenes/Samoa.jpg'),
-(74, 'Sudáfrica', NULL, 0.00, 'Vista/imagenes/Sudafrica.jpg'),
-(75, 'Tailandia', NULL, 0.00, 'Vista/imagenes/Tailandia.jpg');
+(10, 'Canadà', 2, 1400.00, 'Vista/imagenes/Canada.jpg'),
+(12, 'China', 3, 1300.00, 'Vista/imagenes/China.jpg'),
+(13, 'Japón', 3, 1400.00, 'Vista/imagenes/Japon.jpg'),
+(14, 'India', 3, 1000.00, 'Vista/imagenes/India.jpg'),
+(15, 'Corea del Sur', 3, 1200.00, 'Vista/imagenes/CoreaSur.jpg'),
+(16, 'Tailandia', 3, 1100.00, 'Vista/imagenes/Tailandia.jpg'),
+(17, 'Francia', 4, 200.00, 'Vista/imagenes/Francia.jpg'),
+(18, 'España', 4, 150.00, 'Vista/imagenes/Espana.jpg'),
+(19, 'Alemania', 4, 300.00, 'Vista/imagenes/Alemania.jpg'),
+(20, 'Italia', 4, 250.00, 'Vista/imagenes/Italia.jpg'),
+(21, 'Reino Unido', 4, 400.00, 'Vista/imagenes/ReinoUnido.jpg'),
+(22, 'Australia', 5, 1500.00, 'Vista/imagenes/Australia.jpg'),
+(23, 'Nueva Zelanda', 5, 1600.00, 'Vista/imagenes/NuevaZelanda.jpg'),
+(24, 'Fiyi', 5, 1700.00, 'Vista/imagenes/Fiji.jpg'),
+(25, 'Papúa Nueva Guinea', 5, 1800.00, 'Vista/imagenes/NuevaGuinea.jpg'),
+(26, 'Samoa', 5, 1750.00, 'Vista/imagenes/Samoa.jpg');
+
 
 --
 -- Índices para tablas volcadas
@@ -198,7 +157,7 @@ ALTER TABLE `continentes`
 -- AUTO_INCREMENT de la tabla `informacio_viatge`
 --
 ALTER TABLE `informacio_viatge`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 
 --
 -- AUTO_INCREMENT de la tabla `paises`
